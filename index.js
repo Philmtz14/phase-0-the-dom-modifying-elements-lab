c621fd6e7dc2f19332b8b2ddb337674ba0a42872
philip martinez <martinez.philip00@gmail.com>
2022-07-21 22:22:10 -0500

    commit tree e8b9a3540f5c907744a32f9c86cc4a391beba680

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+    //document.getElementById('victory').textContent = `'${Your-Name}' is the champion`;
+    //let text = document.createTextNode(`'${Your-Name}' is the champion`);
+newHeader.id = 'victory';
+
+//let text = document.createTextNode(`'${Your-Name}' is the champion`);
+newHeader.textContent = 'Philip is the champion';
+console.log(newHeader);
+document.body.appendChild(newHeader);
